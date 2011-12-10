d0c65ced79d37938fe6bbc468e1dcdf913fa1e44
dzejkej <jakub.kozisek@gmail.com>
2011-12-10 04:33:10 +0100

    HTML page - added select button, output is now scrollable JavaScript - not loading x-domain ajax from the script, must be a dependency README - changed formatting, added dependency on xdomain jquery plugin

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
-Fiddle Retriever
-================
+JsFiddle Utils
+==============
 
-This is currently a simple JavaScript project allowing to retrieve all fiddles of a specified user from [jsfiddle.net](http://jsfiddle.net). Part of this project is also a page for retrieving and printing all fiddles in stringified JSON format. You can access the page **[HERE](http://dzejkej.github.com/jsfiddle-utils/jsfiddleretriever.html)**.
+This is currently a simple JavaScript project allowing to retrieve all fiddles of a specified user from [jsfiddle.net](http://jsfiddle.net). Part of this project is also a page for retrieving and printing all fiddles in stringified JSON format. You can access the page directly **[HERE](http://dzejkej.github.com/jsfiddle-utils/jsfiddleretriever.html)**.
 
 Data are retrieved using a combination of JSFiddle's own JSONP service (to retrieve fiddle list) and YQL to retrieve a HTML content of every fiddle and save HTML/CSS/JS code.
 
 ### Usage
 
-First of all make sure you have jQuery 1.6.4+ available and that have jsfiddleutils.js included.
+To access *jsfiddle-utils* functionality you need these scripts loaded:
+
+* [jQuery 1.6.4+](http://jquery.com/)
+* [Cross-Domain AJAX jQuery plug-in by James Padosley](https://raw.github.com/padolsey/jQuery-Plugins/master/cross-domain-ajax/jquery.xdomainajax.js) (more info [here](http://james.padolsey.com/javascript/cross-domain-requests-with-jquery/))
+* project's [jsfiddleutils.js](https://raw.github.com/dzejkej/jsfiddle-utils/master/jsfiddleutils.js)
 
 ```javascript
 jsfiddle.retrieve(username, options, callback);
--- jsfiddleutils.js
+++ jsfiddleutils.js
@@ -5,8 +5,6 @@ var jsfiddle = (function($) {
         order: 'asc',
         limit: 100000
     };
-    // url for x-domain .ajax jQuery plugin
-    var xdomurl = 'https://raw.github.com/padolsey/jQuery-Plugins/master/cross-domain-ajax/jquery.xdomainajax.js';
     // "public" object that will get returned
     return {
         retrieve: function(user, options, success) {
@@ -27,32 +25,29 @@ var jsfiddle = (function($) {
                 type: 'GET',
                 data: options,
                 success: function(data) {
-                    // if we are successful then load the x-domain .ajax jQuery plug-in
-                    $.getScript(xdomurl, function() {
-                        var fiddles = data.list, maxCount = fiddles.length, currentCount = 0;
-                        // for each fiddle ..
-                        $.each(fiddles, function(i, fiddle) {
-                            // take the fiddle ID from the url
-                            var fiddleID = idRE.exec(fiddle.url)[1];
-                            // retrieve the fiddle using x-domain plug-in (channeling through YQL)
-                            $.ajax({
-                                url: 'http://jsfiddle.net/' + escape(user) + '/' + fiddleID,
-                                type: 'GET',
-                                success: function(fiddlePage) {
-                                    // retrieve CSS, HTML and JS and save them to the fiddle object
-                                    var $content = $($(fiddlePage.responseText).find('div#content'));
-                                    fiddle.code_js = $content.find('textarea#id_code_js').text();
-                                    fiddle.code_css = $content.find('textarea#id_code_css').text();
-                                    fiddle.code_html = $content.find('textarea#id_code_html').text();
-                                },
-                                complete: function() {
-                                    currentCount++;
-                                    // if we completed all fiddle requests, call success and pass data
-                                    if (currentCount === maxCount) {
-                                        sucess.call(this, fiddles);
-                                    }
+                    var fiddles = data.list, maxCount = fiddles.length, currentCount = 0;
+                    // for each fiddle ..
+                    $.each(fiddles, function(i, fiddle) {
+                        // take the fiddle ID from the url
+                        var fiddleID = idRE.exec(fiddle.url)[1];
+                        // retrieve the fiddle using x-domain plug-in (channeling through YQL)
+                        $.ajax({
+                            url: 'http://jsfiddle.net/' + escape(user) + '/' + fiddleID,
+                            type: 'GET',
+                            success: function(fiddlePage) {
+                                // retrieve CSS, HTML and JS and save them to the fiddle object
+                                var $content = $($(fiddlePage.responseText).find('div#content'));
+                                fiddle.code_js = $content.find('textarea#id_code_js').text();
+                                fiddle.code_css = $content.find('textarea#id_code_css').text();
+                                fiddle.code_html = $content.find('textarea#id_code_html').text();
+                            },
+                            complete: function() {
+                                currentCount++;
+                                // if we completed all fiddle requests, call success and pass data
+                                if (currentCount === maxCount) {
+                                    sucess.call(this, fiddles);
                                 }
-                            });
+                            }
                         });
                     });
                 }
