a53839e028e852073a02a54c99a53e9074c71a24
dzejkej <jakub.kozisek@gmail.com>
2011-12-10 03:10:12 +0100

    intial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+Fiddle Retriever
+================
+
+This is currently a simple JavaScript project allowing to retrieve all fiddles of a specified user from [jsfiddle.net](http://jsfiddle.net). Part of the project is also a page for retrieving and printing all fiddles in stringified JSON format.
+
+Data are retrieved using a combination of JSFiddle's own JSONP service (to retrieve fiddle list) and YQL to retrieve a HTML content of every fiddle and save HTML/CSS/JS code.
+
+### Usage
+
+First of all make sure you have jQuery 1.6.4+ available and that have fiddleutils.js included.
+
+```javascript
+jsfiddle.retrieve(username, options, callback);
+```
+
+*Where*:
+
+* _username_ - name of the user you want to retrieve all fiddles from
+* _options_ - jsfiddle's webservice options (see [documentation](http://doc.jsfiddle.net/api/fiddles.html))
+* _callback(data)_ - callback function that will get called when data are available
+
+*Data* (see [documentation](http://doc.jsfiddle.net/api/fiddles.html)):
+
+```javascript
+  "status": "ok",
+  "list": [
+    {
+      "description": "...",		// description of the fiddle
+      "author": "{username}",	// name of the author
+      "url": "...",				// url of the fiddle
+      "created": "...",			// creation date
+      "framework": "...",		// used framwork
+      "version": 123,           // base version number
+      "latest_version": 234,    // latest version number
+      "title": "..."			// title of the fiddle
+      "code_html": "..."		// HTML code of the fiddle
+      "code_css": "..."			// CSS code of the fiddle
+      "code_js": "..."			// JavaScript code of the fiddle
+    },
+    // ...
+  ],
+  "overallResultSetCount": 123	// number of the results
+```
---
+++ jsfiddleutils.js
@@ -0,0 +1,62 @@
+var jsfiddle = (function($) {
+    // default options for retrieval of fiddles
+    var defaults = {
+        sort: 'date',
+        order: 'asc',
+        limit: 100000
+    };
+    // url for x-domain .ajax jQuery plugin
+    var xdomurl = 'https://raw.github.com/padolsey/jQuery-Plugins/master/cross-domain-ajax/jquery.xdomainajax.js';
+    // "public" object that will get returned
+    return {
+        retrieve: function(user, options, success) {
+            // options were not specified => params are (user, success)
+            if (typeof options === 'function') {
+                sucess = options;
+                options = {};
+            }
+            // merge default options with user options
+            options = $.extend({}, defaults, options);
+            // regexp for extracting fiddle IDs
+            var idRE = new RegExp("/" + escape(user) + "/([^/]+)/");
+            // retrieve list of fiddles using JSONP service
+            $.ajax({
+                // see http://doc.jsfiddle.net/api/fiddles.html
+                url: 'http://jsfiddle.net/api/user/' + escape(user) + '/demo/list.json',
+                dataType: 'jsonp',
+                type: 'GET',
+                data: options,
+                success: function(data) {
+                    // if we are successful then load the x-domain .ajax jQuery plug-in
+                    $.getScript(xdomurl, function() {
+                        var fiddles = data.list, maxCount = fiddles.length, currentCount = 0;
+                        // for each fiddle ..
+                        $.each(fiddles, function(i, fiddle) {
+                            // take the fiddle ID from the url
+                            var fiddleID = idRE.exec(fiddle.url)[1];
+                            // retrieve the fiddle using x-domain plug-in (channeling through YQL)
+                            $.ajax({
+                                url: 'http://jsfiddle.net/' + escape(user) + '/' + fiddleID,
+                                type: 'GET',
+                                success: function(fiddlePage) {
+                                    // retrieve CSS, HTML and JS and save them to the fiddle object
+                                    var $content = $($(fiddlePage.responseText).find('div#content'));
+                                    fiddle.code_js = $content.find('textarea#id_code_js').text();
+                                    fiddle.code_css = $content.find('textarea#id_code_css').text();
+                                    fiddle.code_html = $content.find('textarea#id_code_html').text();
+                                },
+                                complete: function() {
+                                    currentCount++;
+                                    // if we completed all fiddle requests, call success and pass data
+                                    if (currentCount === maxCount) {
+                                        sucess.call(this, fiddles);
+                                    }
+                                }
+                            });
+                        });
+                    });
+                }
+            });
+        }
+    };
+})(jQuery);
